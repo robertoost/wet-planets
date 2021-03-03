0e67be63a1864689bc03549f4e99a14a6ae01771
Robert Oost <robertwilliamoost@gmail.com>
2021-03-03 14:50:08 +0000

    chore: rename script

--- Assets/Scripts/CelestialBody.cs
+++ Assets/Scripts/CelestialBody.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ExertGravitationalForce : MonoBehaviour
+public class CelestialBody : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
